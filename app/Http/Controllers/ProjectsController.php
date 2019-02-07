2fc723a88b9bb6fb4e37b759fdb868a93e9fe38d
Marko Bernatović <markob110584@gmail.com>
2019-02-07 13:33:02 +0100

    Add all files

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = auth()->user()->projects;
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+        if (auth()->user()->isNot($project->owner)) {
+            abort(403);
+        }
+
+        return view('projects.show', compact('project'));
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+//        $attributes['owner_id'] = auth()->id();
+
+        auth()->user()->projects()->create($attributes);
+
+//        Project::create($attributes);
+
+        return redirect('/projects');
+    }
+}
